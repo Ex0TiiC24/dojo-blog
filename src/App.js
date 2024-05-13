901b3d48516319508177f5e16932199e9bcbcafa
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-05-13 17:00:02 +0700

    learning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
+import Navbar from './navbar';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-        <div className="content">
-          <h1>App Component</h1>
-    
-        </div>
+        <Navbar />
         
+        <div className="content">
+          <Home />
+        </div>
     </div>
   );
 }
